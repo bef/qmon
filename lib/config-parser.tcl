93d2320047cae984cc86dd215ff086a37be8b102
BeF <bef@pentaphase.de>
2014-01-22 16:35:13 +0000

    new ini parser

--- lib/config-parser.tcl
+++ lib/config-parser.tcl
@@ -1,59 +1,75 @@
-package require inifile
+source [file join [file dirname [info script]] ini-parser.tcl]
 
 proc parse_config {fn} {
-	array set cfg {global.plugin_path {} global.default_interval 3600 checks {}}
-	set default_check {host unknown desc "" enabled 1 cmd {} interval "" type check}
-	set default_host {desc ""}
-	
-	set ini [::ini::open $fn r]
-	foreach s [::ini::sections $ini] {
-		set sdict [::ini::get $ini $s]
-		if {$s ne "global"} {
-			if {![dict exists $sdict type]} {
-				dict set sdict type check
-				# return -code error "section '$s' has no type"
-			}
-			if {[info exists cfg(${s}.type)]} {return -code error "duplicate section '$s'"}
-			switch [dict get $sdict type] {
-				host {
-					set sdict [dict merge $default_host $sdict]
-					if {![dict exists $cfg(checks) $s]} {dict set cfg(checks) $s {}}
-				}
-				check {
-					set sdict [dict merge $default_check $sdict]
-					if {[dict get $sdict desc] eq ""} {dict set sdict desc $s}
-					set h [dict get $sdict host]
-					# if {![dict exists $cfg(checks) $h]} {dict set cfg(checks) $h {}}
-					dict lappend cfg(checks) $h $s
-				}
-				default {return -code error "section '$s' has unknown type"}
-			}
-			# lappend cfg([dict get $sdict type]s) $s
-		}
-		foreach {k v} $sdict {
-			set cfg(${s}.$k) $v
-		}
+	array set cfg {
+		global.plugin_path {}
+		global.default_interval 3600
+		checks {}
 	}
-	::ini::close $ini
+	set default_check {
+		host unknown
+		desc ""
+		enabled 1
+		cmd {}
+		interval ""
+		type check
+	}
+	set default_host {
+		desc ""
+	}
 	
-	##
-	
-	foreach {host checks} $cfg(checks) {
-		foreach check $checks {
-			## subst commands
-			set cmd $cfg(${check}.cmd)
-			set cfg(${check}.cmd) [subst -nocommands $cmd]
-	
-			## set default values
-			if {$cfg(${check}.interval) eq ""} {set cfg(${check}.interval) $cfg(global.default_interval)}
+	foreach {section kv} [::ini2::parse_file $fn] {
+		if {$section ne "global"} {
+			## no type? -> type=check
+			if {![dict exists $kv type]} {
+				dict set kv type check
+			}
 			
+			switch [dict get $kv type] {
+				host {
+					## apply defaults
+					set kv [dict merge $default_host $kv]
+					
+					## create empty check list for host
+					if {![dict exists $cfg(checks) $section]} {dict set cfg(checks) $section {}}
+				}
+
+				check {
+					## apply defaults
+					set kv [dict merge $default_check $kv]
+					if {[dict get $kv desc] eq ""} [dict set kv desc $section]
+					if {[dict get $kv interval] eq ""} [dict set kv interval $cfg(global.default_interval)]
+					
+					## subst cmd
+					dict set kv cmd [subst -nocommands [dict get $kv cmd]]
+					
+					## add check to list of checks by host
+					dict lappend cfg(checks) [dict get $kv host] $section
+				}
+				default {return -code error "section $section's type is incorrect"}
+			}
 		}
+		foreach {k v} $kv {
+			set cfg(${section}.$k) $v
+		}
+		
 	}
+	# ::ini::close $ini
+	
+	## check postprocessing
+	# foreach {host checks} $cfg(checks) {
+	# 	foreach check $checks {
+	# 		if {$cfg(${check}.interval) eq ""} {set cfg(${check}.interval) $cfg(global.default_interval)}
+	# 
+	# 		## subst cmd
+	# 		set cfg(${check}.cmd) [subst -nocommands $cfg(${check}.cmd)]
+	# 	}
+	# }
 	
 	##
 	
 	return [array get cfg]
 }
 
-# puts [parse_config qmon.ini]
+# puts [parse_config [file dirname [info script]]/../x/initest.ini]
 
---
+++ lib/ini-parser.tcl
@@ -0,0 +1,81 @@
+package provide ini2 0.1
+package require fileutil
+
+namespace eval ::ini2 {}
+
+proc ::ini2::parse_file {fn} {
+	set lines [split [fileutil::cat $fn] "\n"]
+	set lines [macroprocessor $lines]
+	return [parse_ini $lines]
+}
+
+## substitute templates
+proc ::ini2::macroprocessor {lines} {
+	array set tmpl {}
+	array set tmpl_argumentnames {}
+	set usecnt 1
+	while {$usecnt} {
+		## $mode is "out" or "tmpl". both array variables must exist
+		set mode out
+		
+		## $out is the list of output lines for each iteration
+		set out {}
+		
+		## $usecnt is how many templates were substituted in each iteration
+		set usecnt 0
+		foreach line $lines {
+			if {[regexp {^#template\s+(.*?)(?:\s+(.*))?$} $line -> template_name arguments]} {
+				set mode tmpl($template_name)
+				set tmpl($template_name) {}
+				set tmpl_argumentnames($template_name) $arguments
+				continue
+			}
+			if {[regexp {^#end\s+template\s*$} $line]} {
+				set mode out
+				continue
+			}
+			if {$mode eq "out" && [regexp {^#use\s+(.*?)(?:\s+(.*))?$} $line -> template_name arguments]} {
+				incr usecnt
+				## apply template
+				if {![info exists tmpl($template_name)]} {return -code error "unknown template '$template_name'"}
+				set mapping [lmap argname $tmpl_argumentnames($template_name) arg $arguments {list $argname $arg}]
+				set mapping [concat {*}$mapping]
+				foreach l $tmpl($template_name) {
+					lappend out [string map $mapping $l]
+				}
+				continue
+			}
+			lappend $mode $line
+		}
+		if {$usecnt} {set lines $out}
+	}
+	return $out
+}
+
+## this is more or less what package inifile would have done.
+proc ::ini2::parse_ini {lines} {
+	set section "global"
+	set out {}
+	foreach line $lines {
+		## ignore comments and empty lines
+		if {[regexp {^\s*;} $line] || [regexp {^\s*$} $line]} {continue}
+		
+		## parse sections
+		if {[regexp {^\[(.*?)\]\s*} $line -> section]} {
+			dict set out $section {}
+			continue
+		}
+		
+		## parse key/value pairs
+		regexp {^(.*?)(?:=(.*))?$} $line -> key value
+		dict lappend out $section $key $value
+	}
+	return $out
+}
+
+# foreach {section kv} [::ini2::parse_file [file dirname [info script]]/initest.ini] {
+# 	puts "\n\[$section\]"
+# 	foreach {k v} $kv {
+# 		puts "$k=$v"
+# 	}
+# }
