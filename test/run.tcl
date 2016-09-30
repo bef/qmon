14591ec94f26f4eb9d2e4efbaf5ac99129c5441b
BeF <bef@pentaphase.de>
2016-09-30 12:00:57 +0200

    tests need tcl 8.6

--- test/run.tcl
+++ test/run.tcl
@@ -1,5 +1,5 @@
 #!/usr/bin/env tclsh
-package require Tcl 8.5
+package require Tcl 8.6
 package require tcltest
 # package require example
 ::tcltest::configure -testdir [file dirname [file normalize [info script]]]
